051a31247f186096ab54c630028584559eb8e968
Ruslan <shadmanov.ruslan@gmail.com>
2024-06-29 18:16:45 -0700

    fixed bugs

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,11 @@ import RecentProject from "@/components/RecentProject";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/ui/Footer";
 
 export default function Home() {
   return (
-    <main className="relative bg-blacknpm-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
+    <main className="relative bg-blacknpm-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -18,6 +19,7 @@ export default function Home() {
         <RecentProject />
         <Clients />
         <Experience />
+        <Footer />
       </div>
     </main>
   );
